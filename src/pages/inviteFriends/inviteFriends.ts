85820a3a0eb41c8d9ab5b5705e82b1facd33757c
Indina Smith <indina@hotmail.no>
2017-11-03 11:06:06 +0100

    prøvde meg på sortering med fobokstav på navn i kontaktliste, er en bugg, noe er feil.

--- src/pages/inviteFriends/inviteFriends.ts
+++ src/pages/inviteFriends/inviteFriends.ts
@@ -8,6 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * on Ionic pages and navigation.
  */
 
+ 
 @IonicPage()
 @Component({
   selector: 'page-inviteFriends',
@@ -16,10 +17,85 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class InviteFriendsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+    this.initializeItems();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InviteFriendsPage');
   }
 
+
+ contacts;
+    groupedContacts = [];
+
+
+ searchQuery: string = '';
+  items: string[];
+
+
+
+  initializeItems() {
+    this.items = [
+      'Indisstyle',
+      'Siripus',
+      'Silje',
+      'Lars',
+      'Fredrik',
+      
+    ];
+    this.groupContacts(this.items);
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+
+
+
+   groupContacts(Items){
+ 
+        let sortedContacts = Items.sort();
+        let currentLetter = false;
+        let currentContacts = [];
+ 
+        sortedContacts.forEach((value, index) => {
+ 
+            if(value.charAt(0) != currentLetter){
+ 
+                currentLetter = value.charAt(0);
+ 
+                let newGroup = {
+                    letter: currentLetter,
+                    item: []
+                };
+ 
+                currentContacts = newGroup.item;
+                this.groupedContacts.push(newGroup);
+ 
+            }
+ 
+            currentContacts.push(value);
+ 
+        });
+ 
+    }
 }
+
+
+
+
+
